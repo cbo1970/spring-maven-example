3a20a89e582e1e4013dd850c67bf6ed8a9e6c169
Bonnhoff, Christian <Christian.Bonnhoff@zuehlke.com>
2021-06-28 10:40:00 +0200

    init commmit

---
+++ src/main/java/com/example/springmavenexample/SpringMavenExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.springmavenexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMavenExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMavenExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springmavenexample/SpringMavenExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springmavenexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMavenExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
